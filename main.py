724c64079f9f9ca0284a2e901702bc1ffd56af4f
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 11:04:13 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Matnlar ro‘yxati. Har bir matndagi barcha unli harflarni " _ " belgisi bilan almashtiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
